0e22fe2f6cb758d279f72380578f32b3fd236e75
James Rickard <james@frodosghost.com>
2020-09-19 10:00:29 +1000

    Team Users endpoint added.
    https://github.com/SaferMe/saferme-api-docs/blob/teams/120_team.md#list-team-users

--- src/Resources/Teams.php
+++ src/Resources/Teams.php
@@ -7,5 +7,20 @@ use SmartOysters\SaferMe\Http\Response;
 
 class Teams extends Resource
 {
+    /**
+     * Get the TeamUsers from team.
+     *
+     * @param int   $team_id Entity ID interact with.
+     * @param array $options
+     * @return Response
+     */
+    public function team_users($team_id, $fields = '', $options = [])
+    {
+        $options = array_merge(
+            compact('team_id', 'fields'),
+            $options
+        );
 
+        return $this->request->get(':team_id/team_users', $options);
+    }
 }
