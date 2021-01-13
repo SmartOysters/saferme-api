7025bd5a1912cc96f9110ff813de699a1f7a9f63
James Rickard <james@frodosghost.com>
2021-01-13 15:16:13 +1100

    Remove Fetch, as we have the get_form function in place.

--- src/Resources/Channels.php
+++ src/Resources/Channels.php
@@ -7,16 +7,6 @@ use SmartOysters\SaferMe\Http\Response;
 
 class Channels extends Resource
 {
-    /**
-     * Fetch the entity details by ID.
-     *
-     * @param int $channel_id Entity ID to find.
-     * @return Response
-     */
-    public function fetch($channel_id)
-    {
-        return $this->request->get(':channel_id/form', compact('channel_id'));
-    }
 
     /**
      * Get the Categories from the set channel.
