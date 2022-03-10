263b678faf55cc090ddf0a7093d6f62c863bc14a
Liam Claridge <liam@smartoysters.com>
2022-03-10 17:21:17 +1100

    File added for Tasks.

---
+++ src/Resources/Tasks.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SmartOysters\SaferMe\Resources;
+
+use SmartOysters\SaferMe\Resources\Base\Resource;
+use SmartOysters\SaferMe\Http\Response;
+
+class Tasks extends Resource
+{
+    //
+}
